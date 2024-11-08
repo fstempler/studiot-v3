cc49453477bb7eeef765adc0eb5f2ab719cdfdc8
fstempler <zackmango@gmail.com>
2024-11-08 14:23:18 -0300

    SUBSCRIBE TEST 4

--- src/Pages/CaseStudies.jsx
+++ src/Pages/CaseStudies.jsx
@@ -17,12 +17,12 @@ const CaseStudies = () => {
             <div className='caseStudies__Hero-Container'>
                 <div className='caseStudies__Hero-Container-Main'>
                     <div className='row p-0 m-0'>
-                        <div className='col-8 col-sm-4 caseStudies__Hero-LeftColumn'>
+                        <div className='col-7 col-sm-4 caseStudies__Hero-LeftColumn'>
                             <h1 className='caseStudies__Title'>Case <br></br>Studies</h1>
                             <Link to="/contact"><YellowCTA text={"Talk To Us"}/></Link> 
                             {/* <img src={star} className='caseStudy__Star' alt='star'/> */}
                         </div>
-                        <div className='col-4 col-sm-8 caseStudies__Hero-RightColumn'>
+                        <div className='col-5 col-sm-8 caseStudies__Hero-RightColumn'>
                             <img src={StudioTSkate} alt='Pink Sparkle' className='caseStudies__StudioTSkate'/>
                         </div>
                     </div>
