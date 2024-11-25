52d3f2cb46872d5d141608ab732348e2fe35a443
fstempler <zackmango@gmail.com>
2024-11-25 17:59:42 -0300

    FIX PRICES PAGE LAST LINE COPY

--- src/Pages/Pricing.jsx
+++ src/Pages/Pricing.jsx
@@ -48,10 +48,6 @@ const Pricing = () => {
             <div className='row pricing__ColumnsContainer'>
                 <div className='col-12 col-sm-12 col-md-4 pricing__LeftColumn'>
                     <div className='pricing__Content-Container pricing__Content-Container-Left'>
-                        {/* <div className='pricing__Title-Container'>
-                            <h2 className='pricing__Title'>Starter</h2>
-                            <p className='pricing__Text'>Existing assets that just need tweaks</p>
-                        </div> */}
                         
                         <div className='pricing__Level-Container'>
                             <div className='pricing__Level'>Level 1</div>
@@ -67,10 +63,6 @@ const Pricing = () => {
                 </div>
                 <div className='col-12 col-sm-12 col-md-4 pricing__MiddleColumn'>
                     <div className='pricing__Content-Container'>
-                        {/* <div className='pricing__Title-Container'>
-                            <h2 className='pricing__Title'>Design Essentials</h2>
-                            <p className='pricing__Text'>Quickly and easily meet your creative needs <br/><br/></p>
-                        </div> */}
                         
                         <div className='pricing__Level-Container'>
                             <div className='pricing__Level'>Level 3</div>
@@ -85,10 +77,6 @@ const Pricing = () => {
                 </div>
                 <div className='col-12 col-sm-12 col-md-4 pricing__RightColumn'>
                     <div className='pricing__Content-Container'>
-                        {/* <div className='pricing__Title-Container'>
-                            <h2 className='pricing__Title'>Digital Advertising</h2>
-                            <p className='pricing__Text'>Creative designed for growth + performance marketing</p>
-                        </div> */}
                         
                         <div className='pricing__Level-Container'>
                             <div className='pricing__Level'>Level 5</div>
@@ -103,7 +91,7 @@ const Pricing = () => {
                 </div>
             </div>
             <div className='row pricing__Row'>
-                <p className='pricing__Row-Content'>Cost per month. Hours roll over for 3 months. Additional hours are $76/credit.</p>
+                <p className='pricing__Row-Content'>Cost per month. Credits roll over for 3 months. Additional credits are $76/credit.</p>
             </div>
             </div>
         </div>
