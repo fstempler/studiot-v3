90a61cddb2cd25623ffd897b2d4c2d917b677c9e
fstempler <zackmango@gmail.com>
2025-04-11 17:45:04 -0300

    REPLACE HOMEPAGE HERO GIF FOR MP4

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,7 +3,9 @@ import './hero.css'
 import ContactCTA from '../yellowCTA/YellowCTA.jsx'
 import { Link } from 'react-router-dom'
 //images
-import testImg from '../../assets/test-img-hero.gif'
+import testImg from '../../assets/test-img-hero.png'
+//video
+import testVid from '../../assets/test-vid-hero.mp4'
 
 
 
@@ -24,11 +26,19 @@ const Hero = () => {
                         <h1 className='title-1'>                                                        
                             <span className='title-2'>When You </span>
                             <span className='hero__Image-Container'>
-                                <img src={testImg} className='hero__Image' /></span>
+                            <video  autoPlay  muted  loop  playsInline  className='hero__Image'>
+                                <source src={testVid} type="video/mp4" />
+                                <img src={testImg} alt="Animación decorativa" />
+                            </video>
+                                
+                            </span>
                             <span className='title-3'> Need It.</span>
                         
                         </h1>                        
-                        <img src={testImg} className='hero__Image-Cel' />
+                            <video  autoPlay  muted  loop  playsInline  width="80%" className='hero__Image-Cel'>
+                                <source src={testVid} type="video/mp4" />
+                                {/* <img src={testImg} alt="Animación decorativa" /> */}
+                            </video>
                         <h2 className='hero__Subtitle'>Your creative team’s BFF: fast, on-brand, performance-driven design without the messy group chats and missed deadlines.</h2>                        
                         <Link to={"/contact"}>
                             <ContactCTA text={"Let's Do This!"}/>
